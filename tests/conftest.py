ef0b1c529a61e8e1cb12663c55e96786001193db
Vladimir Kamarzin <vvk@vvk.pp.ru>
2019-04-27 13:42:49 +0500

    Add initial tests

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+from bitshares import BitShares
+
+@pytest.fixture(scope='session')
+def bitshares():
+    """ Initialize BitShares instance
+    """
+    bitshares = BitShares()
+
+    return bitshares
+
---
+++ tests/test_history.py
@@ -0,0 +1,33 @@
+import pytest
+import json
+
+from download_history import Parser
+
+@pytest.fixture(scope='module')
+def account_name():
+    return 'aleks'
+
+@pytest.fixture(scope='module')
+def parser(bitshares, account_name):
+    return Parser(bitshares, account_name)
+
+@pytest.fixture()
+def transfer_entry():
+    with open('tests/fixture_data/transfer.json') as f:
+        entry = json.load(f)[0]
+    return entry
+
+@pytest.fixture()
+def trade_entry():
+    with open('tests/fixture_data/trade.json') as f:
+        entry = json.load(f)[0]
+    return entry
+
+
+def test_parse_transfer_entry(parser, transfer_entry):
+    data = parser.parse_transfer_entry(transfer_entry)
+    assert data['buy_amount'] > 0
+
+def test_parse_trade_entry(parser, trade_entry):
+    data = parser.parse_trade_entry(trade_entry)
+    assert data['buy_amount'] > 0
