b1b705cca3f6a709a2fbe9b1e1034ef85889a819
Vladimir Kamarzin <vvk@vvk.pp.ru>
2022-09-12 17:02:40 +0500

    Refactor common code in parser test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,6 +5,16 @@ import pytest
 from bitshares_tradehistory_analyzer.parser import Parser
 
 
+def load_full_json(filename):
+    with open(filename) as f:
+        entry = json.load(f)
+    return entry
+
+
+def load_first_json_entry(filename):
+    return load_full_json(filename)[0]
+
+
 @pytest.fixture(scope='module')
 def account_name():
     return 'aleks'
@@ -17,30 +27,22 @@ def parser(bitshares, account_name):
 
 @pytest.fixture()
 def transfer_entry():
-    with open('tests/fixture_data/transfer.json') as f:
-        entry = json.load(f)[0]
-    return entry
+    return load_first_json_entry('tests/fixture_data/transfer.json')
 
 
 @pytest.fixture()
 def transfer_entry_null_op_object():
-    with open('tests/fixture_data/transfer_null_op_object.json') as f:
-        entry = json.load(f)
-    return entry
+    return load_full_json('tests/fixture_data/transfer_null_op_object.json')
 
 
 @pytest.fixture()
 def trade_entry():
-    with open('tests/fixture_data/trade.json') as f:
-        entry = json.load(f)[0]
-    return entry
+    return load_first_json_entry('tests/fixture_data/trade.json')
 
 
 @pytest.fixture()
 def trade_entry_null_op_object():
-    with open('tests/fixture_data/trade_null_op_object.json') as f:
-        entry = json.load(f)
-    return entry
+    return load_full_json('tests/fixture_data/trade_null_op_object.json')
 
 
 def test_parse_transfer_entry(parser, transfer_entry, transfer_entry_null_op_object):
