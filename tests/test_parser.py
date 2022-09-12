ee2a17ad230b129d7ed48f9edffbcd60f79ecea4
Vladimir Kamarzin <vvk@vvk.pp.ru>
2022-09-12 18:07:49 +0500

    Add support for GS-assets settlements parsing

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -45,6 +45,16 @@ def trade_entry_null_op_object():
     return load_full_json('tests/fixture_data/trade_null_op_object.json')
 
 
+@pytest.fixture()
+def settlement_regular_entry():
+    return load_first_json_entry('tests/fixture_data/settlement_regular.json')
+
+
+@pytest.fixture()
+def settlement_gs_entry():
+    return load_first_json_entry('tests/fixture_data/settlement_gs.json')
+
+
 def test_parse_transfer_entry(parser, transfer_entry, transfer_entry_null_op_object):
     data = parser.parse_transfer_entry(transfer_entry)
     assert data['buy_amount'] > 0
@@ -59,3 +69,14 @@ def test_parse_trade_entry(parser, trade_entry, trade_entry_null_op_object):
 
     data = parser.parse_trade_entry(trade_entry_null_op_object)
     assert data['buy_amount'] > 0
+
+
+def test_parse_regular_settle_entry(parser, settlement_regular_entry):
+    data = parser.parse_settle_entry(settlement_regular_entry)
+    assert data['buy_amount'] > 0
+
+
+def test_parse_gs_settle_entry(parser, settlement_gs_entry):
+    data = parser.parse_settle_entry(settlement_gs_entry)
+    assert data['buy_amount'] > 0
+    assert data['sell_amount'] > 0
